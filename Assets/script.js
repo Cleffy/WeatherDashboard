8aa585c0a140c3e8faf45137f37f9b17628664f7
Cleffy <falchard@live.com>
2023-06-11 21:20:14 -0700

    Finished Search Function
    Corrected error with protected word, added all search functionality

--- Assets/script.js
+++ Assets/script.js
@@ -9,16 +9,17 @@ class weather{
 
     }
 }
-var history = [];               //An array of up to 10 previous cities.
-var today = new weather();      //Weather for today
-var dayPlus1 = new weather();   //Weather for today + 1
-var dayPlus2 = new weather();   //Weather for today + 2
-var dayPlus3 = new weather();   //Weather for today + 3
-var dayPlus4 = new weather();   //Weather for today + 4
-var dayPlus5 = new weather();   //Weather for today + 5
+var prevCity = new Array();                              //An array of up to 10 previous cities.
+var today = new weather();                              //Weather for today
+var dayPlus1 = new weather();                           //Weather for today + 1
+var dayPlus2 = new weather();                           //Weather for today + 2
+var dayPlus3 = new weather();                           //Weather for today + 3
+var dayPlus4 = new weather();                           //Weather for today + 4
+var dayPlus5 = new weather();                           //Weather for today + 5
+var searchEl = document.getElementById("citySearch");   //form element
 
 function setHistory(){
-    localStorage.setItem("weather", JSON.stringify(history));
+    localStorage.setItem("weather", JSON.stringify(prevCity));
 }
 function clearHistory(){
     localStorage.removeItem("weather");
@@ -26,35 +27,42 @@ function clearHistory(){
 function displayHistory(){
     let historyEl = document.getElementById("history");
     historyEl.innerHTML = '';
-    for(let index = 0; index < history.length; index++){
+    if(prevCity.length <= 0){
+        return;
+    }
+    for(let index = 0; index < prevCity.length; index++){
         let cityEl = document.createElement('button');
-        cityEl.text = history[index];
-        cityEl.onclick = "getWeather(" + history[index] + ")";
+        cityEl.textContent = prevCity[index];
+        cityEl.addEventListener('click', function(event){
+            getWeather(prevCity[index]);
+        });
         let cancelEl = document.createElement('i');
         cancelEl.className = "fas fa-window-close";
-        cancelEl.onclick = "deleteHistory(" + index + ")";
+        cancelEl.addEventListener('click', function(event){
+            deleteHistory(index);
+        });
         cityEl.appendChild(cancelEl);
         historyEl.appendChild(cityEl);
     }
 }
 function getHistory(){
-    var tempHistory = localStorage.getItem("weather");
-    if(tempHistory != null){
-        history = JSON.parse(tempHistory);
-    }
-    else{
-        history = [];
+    prevCity = JSON.parse(localStorage.getItem("weather"));
+    if(prevCity == null){
+        prevCity = new Array();
     }
     displayHistory();
 }
 function addHistory(city){
-    history.push(city);
+    prevCity.push(city);
+    if(prevCity.length > 10){
+        deleteHistory(0);
+    }
     setHistory();
     displayHistory();
 }
 function deleteHistory(key){
-    history.splice(key, 1);
-    if(history.length > 0){
+    prevCity.splice(key, 1);
+    if(prevCity.length > 0){
         setHistory();
     }
     else{
@@ -63,7 +71,17 @@ function deleteHistory(key){
     displayHistory();
 }
 
+function citySearch(event) {
+    event.preventDefault();
+    let cityInput = document.getElementById("cityInput");
+    getWeather(cityInput.value);
+    addHistory(cityInput.value);
+    cityInput.value = '';
+}
+searchEl.addEventListener("submit", citySearch);
 
 function getWeather(city){
 
 }
+
+getHistory();
