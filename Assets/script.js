c1b070b1eebd77819c00fc7b5609d8ab0bf62d46
Cleffy <falchard@live.com>
2023-06-11 18:02:52 -0700

    Update script.js
    Added necessary functions

---
+++ Assets/script.js
@@ -0,0 +1,21 @@
+class weather{
+    city;
+    day;
+    condition;
+    temp;
+    wind;
+    humidity;
+    constructor(){
+
+    }
+}
+var history;
+function loadHistory(){
+
+}
+function addHistory(key){
+
+}
+function deleteHistory(key){
+
+}
