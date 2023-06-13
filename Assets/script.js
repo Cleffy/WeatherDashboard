9587d71e22f787baf7799ccf638afc94cbb41124
Cleffy <falchard@live.com>
2023-06-12 23:59:44 -0700

    Styled the site
    Finished Styling

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,47 @@
+/*
+    Data Object for information I need on weather.
+*/
 class weather{
-    day;
-    condition;
-    conditionIcon;
-    temp;
-    wind;
-    humidity;
+    day;                //String day formatted by dayjs
+    condition;          //String OpenWeatherMap API Condition
+    conditionIcon;      //String OpenWeatherMap API Link to image icon
+    temp;               //String OpenWeatherMap API Formatted temp
+    wind;               //String OpenWeatherMap API Formatted wind speed
+    humidity;           //String OpenWeatherMap API Formatted humidity
     constructor(){
 
     }
 }
+/*
+prevCity = Array of strings of previous city searches
+weekForecase = Array of weather objects [Today], ... [5 days ahead]
+searchEl = Dom element to search form
+*/
 var prevCity = new Array();
 var weekForecast = [new weather(), new weather(), new weather(), new weather(), new weather(), new weather()];
 var searchEl = document.getElementById("citySearch");
-const weatherAPIKey;
-const bingMapsAPIKey;
 
+//Keys displayed for functionality test. Will be deleted after.
+const weatherAPIKey = "1baaf4b6d9aa798b3b8d3da12f59ef1f";
+const bingMapsAPIKey = "AqBjs9NHGIEZvYOeZEnKxKECXHpOHtdfQFrvkMwLm4iGlk5-il_6PI1U6c5Bwu9s";
+
+/*
+Initialization
+Initialize form submit event
+Build history tab
+*/
 searchEl.addEventListener("submit", citySearch);
 displayHistory();
 
+/*
+Functions working with local storage
+setHistory() - puts prevCity into local storage
+clearHistory() - removes prevCity data from local storage
+getHistory() - adds local storage data to prevCity
+addHistory(city) - adds a city to prevCity and updates local storage
+deleteHistory(key) - removes a city from prevCity and updates local storage
+displayHistory() - populates prevCity, clears the site area, adds prevCity data to site
+*/
 function setHistory(){
     localStorage.setItem("weather", JSON.stringify(prevCity));
     displayHistory();
@@ -25,6 +49,29 @@ function setHistory(){
 function clearHistory(){
     localStorage.removeItem("weather");
     displayHistory();
+}
+function getHistory(){
+    prevCity = JSON.parse(localStorage.getItem("weather"));
+    if(prevCity == null){
+        prevCity = new Array();
+    }
+}
+function addHistory(city){
+    prevCity.push(city);
+    if(prevCity.length > 10){
+        deleteHistory(0);
+        return;
+    }
+    setHistory();
+}
+function deleteHistory(key){
+    prevCity.splice(key, 1);
+    if(prevCity.length > 0){
+        setHistory();
+    }
+    else{
+        clearHistory();
+    }
 }
 function displayHistory(){
     getHistory();
@@ -55,46 +102,43 @@ function displayHistory(){
     clearEl.addEventListener('click', clearHistory);
     historyEl.appendChild(clearEl);
 }
-function getHistory(){
-    prevCity = JSON.parse(localStorage.getItem("weather"));
-    if(prevCity == null){
-        prevCity = new Array();
-    }
-}
-function addHistory(city){
-    prevCity.push(city);
-    if(prevCity.length > 10){
-        deleteHistory(0);
-        return;
-    }
-    setHistory();
-}
-function deleteHistory(key){
-    prevCity.splice(key, 1);
-    if(prevCity.length > 0){
-        setHistory();
-    }
-    else{
-        clearHistory();
-    }
-}
 
-function citySearch(event) {
+
+/*
+    citySearch event
+    Check to see if it's a valid city
+    Get Weather data
+    Add the city to history
+    Clear the text element
+*/
+async function citySearch(event) {
     event.preventDefault();
     let cityInput = document.getElementById("cityInput");
-    getWeather(cityInput.value);
-    addHistory(cityInput.value);
-    cityInput.value = '';
+    if(await getWeather(cityInput.value)){
+        addHistory(cityInput.value);    
+        cityInput.value = '';
+    }
 }
+/*
+    getWeather(city)
+    returns true if it successfully completes, otherwise it returns false
+    Get's geocoordinates from Bing Maps API.
+    Get's weather information from OpenWeatherMap API.
 
+    ToDo: Fetch API call from backend rather than frontend
+*/
 async function getWeather(city){
-    let rawLocation = await fetch("http://dev.virtualearth.net/REST/v1/Locations?query=" + city + "&include=queryParse&key=" + bingMapsAPIKey + "&output=json");
-    let geoLocation = await rawLocation.json();
+    let geoLocation = await fetchData("http://dev.virtualearth.net/REST/v1/Locations?query=" + city + "&include=queryParse&key=" + bingMapsAPIKey + "&output=json");
+    if(geoLocation.statusCode == 401){
+        return false;
+    }
     let lat = geoLocation.resourceSets[0].resources[0].geocodePoints[0].coordinates[0];
     let lon = geoLocation.resourceSets[0].resources[0].geocodePoints[0].coordinates[1];
-    let rawForecast = await fetch("http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + weatherAPIKey);
-    let forecast = await rawForecast.json();
-    //console.log(forecast);
+
+    let forecast = await fetchData("http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + weatherAPIKey);
+    if(forecast.statusCode == 401){
+        return false;
+    }
     for(let index in weekForecast){
         weekForecast[index].day = dayjs().add(index, 'day');
         weekForecast[index].condition = forecast.list[index].weather[0].description;
@@ -103,10 +147,13 @@ async function getWeather(city){
         weekForecast[index].wind = forecast.list[index].wind.speed.toFixed(2) + " MPH";
         weekForecast[index].humidity = forecast.list[index].main.humidity + " %";
     }
+    document.getElementById("mainContainer").style.display = "block";
     displayToday(city);
     displayForecast();
+    return true;
 }
 
+//Adds today's weather information to site
 function displayToday(city){
     let currentWeather = document.getElementById("currentConditions");
     currentWeather.innerHTML = '';
@@ -132,6 +179,7 @@ function displayToday(city){
     currentWeather.appendChild(currentWind);
     currentWeather.appendChild(currentHumidity);
 }
+//Adds five day weather forecast to the site
 function displayForecast(){
     for(let index = 1; index < weekForecast.length; index++){
         let thisWeather = document.getElementById("day+" + index);
@@ -157,6 +205,12 @@ function displayForecast(){
     }
 }
 
+//Helper Function to fetch data
+async function fetchData(target){
+    let response = await fetch(target);
+    return await response.json();
+}
+//Helper Function convert kelvin to Fahrenheit
 function kelvinToFahrenheit(kelvin){
     return (kelvin * 1.8 -459.67);
 }
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Weather Dashboard
  Weather outlook for multiple cities
+
+## Description
+This site searches for the current 5-day weather forecast for a city, and saves up to 10 cities in case the user wants to search for them again.
+
+## Usage
+https://cleffy.github.io/WeatherDashboard/
+
+![Five day weather forecast preview](./Assets/WeatherDashboardPreview.jpeg)
+
+The forecast area is originally blank until the user searches for a city.  
+When the user searches for a city, it saves the query on their local storage. It searches Bing Maps for the geolocation of the city, then it requests the weather information from Open Weather Map API. It displays today's weather and the 5-day forecast.  
+The user can research a city by clicking the corresponding button.  
+If the user clicks the X icon, it clears that city from history.  
+If the user clicks the clear button, it clears the entire history.
+
+## Known Issues
+Since this webpage does not have a backend, the API keys to use the services are exposed. Once the issue is resolved, the keys will be removed.
+
+## Credits
+Open Weather Map API - https://openweathermap.org/forecast5  
+Bing Maps Locations API - https://learn.microsoft.com/en-us/bingmaps/rest-services/locations/  
+Bootstrap - https://getbootstrap.com/  
+Font Awesome - https://fontawesome.com/  
+Day.js - https://day.js.org/  
+W3Schools - https://www.w3schools.com/  
+Mozilla Developer Resource - https://developer.mozilla.org/en-US/
